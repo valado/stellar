b659f5f228e70b8ae28120491a256ba0b64ea224
wgumenyuk <wlad@gumenyuk.de>
2025-04-08 12:08:41 +0200

    feat: improve error handling

--- src/components/Scene/index.tsx
+++ src/components/Scene/index.tsx
@@ -1,24 +1,70 @@
 import { Canvas } from "@react-three/fiber";
-import { XR, createXRStore } from "@react-three/xr";
-import { FC, useState } from "react";
+import { XR, createXRStore, XRHandModel } from "@react-three/xr";
+import { FC, Suspense, useEffect, useState } from "react";
 
-const store = createXRStore();
+const store = createXRStore({
+  hand: XRHandModel,
+  handTracking: true,
+});
 
 export const Scene: FC = () => {
+  const [supported, setSupported] = useState(false);
+  const [ar, setAR] = useState(false);
   const [red, setRed] = useState(false);
+
+  const enterAR = async () => {
+    try {
+      const session = await store.enterAR();
+
+      if (!session) {
+        // TODO: Fehler anzeigen.
+        return;
+      }
+
+      session.onend = () => {
+        setAR(false);
+        console.log("AR session ended!");
+      };
+
+      setAR(true);
+      console.log("AR session started!");
+    } catch (err: unknown) {
+      console.error(err);
+      // TODO: Fehler anzeigen.
+    }
+  };
+
+  useEffect(() => {
+    // Bessere Erkennung für den Support von AR.
+    (async () => {
+      console.log("Checking support for AR...");
+      setSupported(
+        "xr" in navigator &&
+          (await navigator.xr!.isSessionSupported("immersive-ar"))
+      );
+    })();
+  }, []);
+
   return (
     <>
-      <button onClick={() => store.enterAR()}>Enter AR</button>
+      {!ar && (
+        <span style={{ display: "block" }}>
+          Supported: {JSON.stringify(supported)}
+        </span>
+      )}
+      {!ar && <button onClick={enterAR}>Enter AR</button>}
       <Canvas>
         <XR store={store}>
-          <mesh
-            pointerEventsType={{ deny: "grab" }}
-            onClick={() => setRed(!red)}
-            position={[0, 1, -1]}
-          >
-            <boxGeometry />
-            <meshBasicMaterial color={red ? "red" : "blue"} />
-          </mesh>
+          <Suspense>
+            <mesh
+              pointerEventsType={{ deny: "grab" }}
+              onClick={() => setRed(!red)}
+              position={[0, 1, -1]}
+            >
+              <boxGeometry />
+              <meshBasicMaterial color={red ? "red" : "blue"} />
+            </mesh>
+          </Suspense>
         </XR>
       </Canvas>
     </>
