ca63b12f17ae4e034a23d16c41d82382e7044f08
wgumenyuk <wlad@gumenyuk.de>
2025-04-08 12:08:13 +0200

    chore: enable HTTPS for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// @ts-expect-error
+import fs from "node:fs";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+  },
+});
