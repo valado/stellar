2db4fbe4621d2b1a38edb80eb100ea6091afa3f0
wgumenyuk <wlad@gumenyuk.de>
2025-04-11 13:17:12 +0200

    refactor: add hit testing (WIP)

--- src/components/Scene/index.tsx
+++ src/components/Scene/index.tsx
@@ -1,65 +1,58 @@
 import { Canvas } from "@react-three/fiber";
-import { XR, createXRStore, XRHandModel } from "@react-three/xr";
-import { FC, Suspense, useEffect, useState } from "react";
-import { MediaPipeHandTracker } from "./MediaPipeHandTracker.tsx";
-import { useRef } from "react";
-import * as THREE from "three";
-
+import {
+  IfInSessionMode,
+  XR,
+  XRDomOverlay,
+  createXRStore,
+} from "@react-three/xr";
+import { FC, useEffect, useState } from "react";
 
 const store = createXRStore({
-  hand: XRHandModel,
-  handTracking: true,
+  domOverlay: true,
+  hitTest: true,
+  anchors: true,
+  layers: true,
 });
 
 export const Scene: FC = () => {
-  const meshRef = useRef<THREE.Mesh>(null);
-
   const [supported, setSupported] = useState(false);
-  const [useMediaPipe, setUseMediaPipe] = useState(false);
-
   const [ar, setAR] = useState(false);
-  const [red, setRed] = useState(false);
 
   const enterAR = async () => {
     try {
       const session = await store.enterAR();
 
       if (!session) {
-        // TODO: Fehler anzeigen.
+        console.error("[Session]", "Failed to retrieve AR session.");
         return;
       }
 
       session.onend = () => {
         setAR(false);
-        console.log("AR session ended!");
+        console.log("[Session]", "Ending current AR session.");
       };
 
       setAR(true);
-      console.log("AR session started!");
+      console.log("[Session]", "Starting new AR session.");
     } catch (err: unknown) {
-      console.error(err);
-      // TODO: Fehler anzeigen.
+      console.error("[Session]", "Failed to retrieve AR session.", err);
     }
+  };
+
+  const exitAR = () => {
+    const { session } = store.getState();
+
+    if (!session) {
+      return;
+    }
+
+    session.end();
   };
 
   useEffect(() => {
     (async () => {
-      const arSupported = "xr" in navigator && await navigator.xr!.isSessionSupported("immersive-ar");
-      setSupported(arSupported);
-  
-      if (!arSupported || !(navigator as any).xr?.hand) {
-        setUseMediaPipe(true); // fallback to MediaPipe
-      }
-    })();
-  }, []);
-  
-
-  useEffect(() => {
-    // Bessere Erkennung für den Support von AR.
-    (async () => {
-      console.log("Checking support for AR...");
       setSupported(
-        "xr" in navigator &&
+        !!navigator.xr &&
           (await navigator.xr!.isSessionSupported("immersive-ar"))
       );
     })();
@@ -68,37 +61,22 @@ export const Scene: FC = () => {
   return (
     <>
       {!ar && (
-        <span style={{ display: "block" }}>
-          Supported: {JSON.stringify(supported)}
-        </span>
+        <>
+          <span style={{ display: "block" }}>
+            Supported: {JSON.stringify(supported)}
+          </span>
+          <button onClick={enterAR}>Enter AR</button>
+        </>
       )}
-      {!ar && <button onClick={enterAR}>Enter AR</button>}
       <Canvas>
         <XR store={store}>
-          <Suspense>
-            <mesh
-              ref={meshRef}
-              pointerEventsType={{ deny: "grab" }}
-              onClick={() => setRed(!red)}
-              position={[0, 1, -1]}
-            >
-              <boxGeometry />
-              <meshBasicMaterial color={red ? "red" : "blue"} />
-            </mesh>
-          </Suspense>
+          <IfInSessionMode>
+            <XRDomOverlay>
+              <button onClick={exitAR}>Exit AR</button>
+            </XRDomOverlay>
+          </IfInSessionMode>
         </XR>
       </Canvas>
-      {useMediaPipe && (
-  <MediaPipeHandTracker
-    onHandMove={([x, y]) => {
-      if (meshRef.current) {
-        meshRef.current.position.x = x;
-        meshRef.current.position.y = y + 1; // +1 to keep it above floor
-      }
-    }}
-  />
-)}
-
     </>
   );
 };
