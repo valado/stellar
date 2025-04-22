0711908f8222cd1a170c7a805e2f6313234c8007
wgumenyuk <wlad@gumenyuk.de>
2025-04-22 15:04:17 +0200

    feat: add border radius to `Card` component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,6 +7,7 @@ export const Card: FC<PropsWithChildren> = ({ children }) => (
         "radial-gradient(113% 91% at 17% -2%, #888888 1%, #FF000000 99%),radial-gradient(75% 75% at 50% 50%, #484848 0%, #606060 100%)",
       padding: "1em",
       border: "1px solid #484848",
+      borderRadius: "1em",
     }}
   >
     {children}
