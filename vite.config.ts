c43bcbe1523e22742d1ca6b06fecc18c71ae0f42
wgumenyuk <wlad@gumenyuk.de>
2025-04-23 11:17:05 +0200

    chore: use HTTPS only for development

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import fs from "node:fs";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// @ts-expect-error: `process` is a Node.js global.
+const dev = process.env.NODE_ENV !== "production";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync("ca/key.pem"),
-      cert: fs.readFileSync("ca/cert.pem"),
-    },
+    https: dev
+      ? {
+          key: fs.readFileSync("ca/key.pem"),
+          cert: fs.readFileSync("ca/cert.pem"),
+        }
+      : undefined,
   },
 });
