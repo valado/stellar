780ad7f1fe026460aaaafbce2d0647b611398e5a
Dmytro Shchurovskyi <dmytro.shchurovskyi@soprasteria.com>
2025-04-30 15:35:39 +0200

    fix: changed path to earth.tsx

--- src/components/demo/finance/Scene.tsx
+++ src/components/demo/finance/Scene.tsx
@@ -2,21 +2,16 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { IfInSessionMode, XR } from "@react-three/xr";
 import { Environment } from "@react-three/drei";
-import { UI } from "$components/demo/house/UI";
-import { HitTest } from "$components/HitTest";
-import { House } from "$components/demo/house/House";
 
 // Stores
 import { useXRSession } from "$stores/xr-session";
-import { usePose } from "$stores/pose";
 
 // Types
 import type { FC } from "react";
-import { Earth } from "./FinancialData/Earth";
+import { Earth } from "./views/Earth";
 
 export const Scene: FC = () => {
   const store = useXRSession((state) => state.store);
-  const isPoseSet = usePose((state) => state.isPoseSet);
 
   return (
     <Canvas>
