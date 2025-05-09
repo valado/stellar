b5d6c416b68ce81635d47b02428e8d5a084261d2
wgumenyuk <wlad@gumenyuk.de>
2025-05-09 16:20:23 +0200

    feat: use dark mode for XR UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { createDemo } from "$factories/demo";
+import { setPreferredColorScheme } from "@react-three/uikit";
 
 // Components
 import { StrictMode } from "react";
 import { RouterProvider } from "react-router";
+import { HydrateFallback } from "$components/HydrateFallback";
+import { MainPage } from "$components/MainPage";
 
 // CSS
 import "./index.css";
-import { MainPage } from "$components/MainPage";
-import { HydrateFallback } from "$components/HydrateFallback";
+
+setPreferredColorScheme("dark");
 
 const router = createBrowserRouter([
   {
