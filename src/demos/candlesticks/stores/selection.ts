cc2c0605cf8591f61919dd8dbc8d7cbe658b0c43
wgumenyuk <wlad@gumenyuk.de>
2025-05-09 14:24:43 +0200

    feat: reset selection on exit

--- src/demos/candlesticks/stores/selection.ts
+++ src/demos/candlesticks/stores/selection.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 type SelectionStore = {
   selection: string;
   setSelection: (selection: string) => void;
+  resetSelection: () => void;
 };
 
+const DEFAULT_SECTION = "AAPL";
+
 export const useSelection = create<SelectionStore>((set) => ({
-  selection: "AAPL",
+  selection: DEFAULT_SECTION,
   setSelection: (selection: string) => set({ selection }),
+  resetSelection: () => set({ selection: DEFAULT_SECTION }),
 }));
