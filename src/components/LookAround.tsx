bc818e3c21438489dcd5700d1543e45d425b9e94
wgumenyuk <wlad@gumenyuk.de>
2025-05-12 11:04:20 +0200

    feat: add `LookAround` component and animation

---
+++ src/components/LookAround.tsx
@@ -0,0 +1,10 @@
+import { SmartphoneIcon } from "lucide-react";
+
+// Types
+import type { FC } from "react";
+
+export const LookAround: FC = () => (
+  <div className="w-8 h-8 mx-auto animate-look-around pointer-events-none">
+    <SmartphoneIcon />
+  </div>
+);
